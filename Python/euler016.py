ee0e193e2b23a0f02fd68ac8407f7dc9dc785c90
patricknaylor <65618586+patricknaylor@users.noreply.github.com>
2022-08-05 23:12:09 -0400

    Create euler016.py

---
+++ Python/euler016.py
@@ -0,0 +1,16 @@
+#########################################
+#
+#Project Euler Problem 16
+#
+#2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+#What is the sum of the digits of the number 2^1000?
+#########################################
+
+#this one is quick in python!
+def sum_of_digits(number):
+    return sum([int(i) for i in str(number)])
+
+print("The sum of the digits is {}.".format(sum_of_digits(2**1000)))
+
+#The answer is 1366.
+
