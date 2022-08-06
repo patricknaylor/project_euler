23d253fc1dce9e7e54605e074af78d906946e5cd
patricknaylor <65618586+patricknaylor@users.noreply.github.com>
2022-08-05 23:31:18 -0400

    Create euler15.py

---
+++ Python/euler15.py
@@ -0,0 +1,21 @@
+########################################
+#Project Euler Problem 15
+#
+#Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down,
+#there are exactly 6 routes to the bottom right corner.
+#How many such routes are there through a 20×20 grid?
+########################################
+
+
+#some math helps us out a lot here! This is a standard counting question.
+#re label things so that we start at the origin, and head out towards the point (20,20).
+#The number of paths from (i,j) to the origin is the sum of the paths from (i+1,j) and (i,j+1)
+#moreover, there's only one path from a point of the form (i,20) or (20,i).
+#In other words, the number of paths is a pascal triangle! The number of paths from (i,j) to the origin is just i choose i+j
+
+#thus the answer is 20 choose 40;
+from math import comb
+
+print("There are {} such paths.".format(comb(40,20)))
+
+#The answer is equal to 137846528820 
