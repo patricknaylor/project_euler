63387b86c8e4a39f0460428fed7779547ba6de64
patricknaylor <65618586+patricknaylor@users.noreply.github.com>
2022-08-25 15:35:50 +0300

    updates

--- Python/euler001.py
+++ Python/euler001.py
@@ -1,25 +1,22 @@
 ################################################
-#Project Euler Problem 1
-
-#If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-#The sum of these multiples is 23.
-#Find the sum of all the multiples of 3 or 5 below 1000.
+#
+# Project Euler Problem 1
+#
+# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+# The sum of these multiples is 23.
+# Find the sum of all the multiples of 3 or 5 below 1000.
+#
 ################################################
 
-
-#we can do this by simply counting these numbers
-
+# we can do this by simply counting these numbers (the bound is small, so we won't worry about speed).
 def sum_integers(bound):
 
     count = 0
     for i in range(bound):
-        if i%3 == 0 or i%5 == 0:
+        if i % 3 == 0 or i % 5 == 0:
             count += i
             
     return count
 
 
-
-print(sum_integers(1000))
-
-#The answer is 233168
+print("The sum of these numbers is {}.".format(sum_integers(1000)))
--- Python/euler004.py
+++ Python/euler004.py
@@ -1,14 +1,15 @@
 ###########################################
-#Project Euler Problem 4
 #
-#A palindromic number reads the same both ways.
-#The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
-
-#Find the largest palindrome made from the product of two 3-digit numbers.
+# Project Euler Problem 4
+#
+# A palindromic number reads the same both ways.
+# The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+#
+# Find the largest palindrome made from the product of two 3-digit numbers.
+#
 ###########################################
 
-
-#1000 isn't so large, so this can be solved by brute force
+# 1000 isn't so large, so this can be solved by brute force
 
 def find_palindrome(bound):
     largest = 0
@@ -16,11 +17,11 @@ def find_palindrome(bound):
     for i in range(1000):
         for j in range(1000):
             if i*j > largest:
-                if str(i*j)[::-1] == str(i*j): #iterate over all pairs of integers; using a useful trick in python to reverse a string
+                # iterate over all pairs of integers; using a useful trick in python to reverse a string
+                if str(i*j)[::-1] == str(i*j):
                     largest = i*j
 
     return largest
 
-print("The largest such palindrome is {}.".format(find_palindrome(1000)))
 
-#The answer is 906609
+print("The largest such palindrome is {}.".format(find_palindrome(1000)))
